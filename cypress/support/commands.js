3f2159f94a1c35e71f0d5f65c930112c5f19343d
Afonso Santos <contatoafonsohenrique@gmail.com>
2024-01-15 11:31:21 -0300

    CACTAT-9 Add Custom Commands to be used in Submit form using Custom Command Test

---
+++ cypress/support/commands.js
@@ -0,0 +1,19 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get("input[id='firstName']").type('Afonso')
+    cy.get("input[id='lastName']").type('Santos')
+    cy.get("input[id='email']").type('contatoafonsohenrique@gmail.com')
+    cy.get("textarea[id='open-text-area']").type('I can not access my course', {delay: 0})
+    cy.clickSubmitButton()
+})
+
+Cypress.Commands.add('clickSubmitButton', function() {
+    cy.contains("button" ,"Enviar").click()
+})
+
+Cypress.Commands.add('viewErrorMessage', function() {
+    cy.get("span[class='success']").should('be.visible')
+})
+
+Cypress.Commands.add('viewSuccesfullMessage', function() {
+    cy.get("span[class='error']").should('be.visible')
+})
