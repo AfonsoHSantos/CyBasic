101c0d3633991348223d877056ee4390add06027
Afonso Santos <contatoafonsohenrique@gmail.com>
2024-01-15 11:28:33 -0300

    CACTAT-3 Add Fill mandatory fields and submit form Test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,5 +9,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
+    it('CACTAT-3: Fill mandatory fields and submit form', function() {
+        cy.get("input[id='firstName']").type('Afonso')
+
+        cy.get("input[id='lastName']").type('Santos')
+
+        cy.get("input[id='email']").type('contatoafonsohenrique@gmail.com')
+
+        cy.get("textarea[id='open-text-area']").type('I can not access my course', {delay: 0})
+        
+        cy.clickSubmitButton()
+
+        cy.viewErrorMessage()
+    })
+
 })
 
