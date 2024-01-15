7ac959fdfe68349e80cfb0a30e326a378fa5380c
Afonso Santos <contatoafonsohenrique@gmail.com>
2024-01-15 11:28:59 -0300

    CACTAT-4 Add Error message is displayed when email with whrong format is added Test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -23,5 +23,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.viewErrorMessage()
     })
 
+    it('CACTAT-4: Error message is displayed when email with whrong format is added', function() {
+        cy.get("input[id='firstName']").type('Afonso')
+
+        cy.get("input[id='lastName']").type('Santos')
+
+        cy.get("input[id='email']").type('invalid.mail')
+
+        cy.get("textarea[id='open-text-area']").type('I can not access my course', {delay: 0})
+        
+        cy.clickSubmitButton()
+
+        cy.viewSuccesfullMessage()
+    })
 })
 
